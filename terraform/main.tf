019cee9054034733e096373e2d1da9b2e07021d2
Dan Maxim <dmaxim@mxinformatics.com>
2021-06-23 14:36:49 -0600

    Add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+# Resource group
+resource "azurerm_resource_group" "wimc-net-rg" {
+    name = join("-", ["rg", var.namespace, var.environment])
+    location = var.location
+}
+
+
+# Create Azure Storage for DAPI
+
+
+# Create Key Vault
+
+
+# Create DAPI Key
---
+++ terraform/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+    backend "remote" {
+        organization = "MXInformatics"
+    }
+
+    workspaces = {
+        name = "wimc-net"
+    }
+
+}
+
+
+provider "azurerm" {
+    subscription_id = var.azure-subscription-id
+    client_id = var.azure-service-principal-id
+    client_secret = var.azure-service-principal-secret
+    tenant_id = var.azure-tenant-id
+    features {}
+}
