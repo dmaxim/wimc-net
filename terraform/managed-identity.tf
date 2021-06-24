6d07bc0e6108abf33b0b39f709996ecb9608e9cc
Dan Maxim <dmaxim@mxinformatics.com>
2021-06-24 11:34:37 -0600

    Try adding required managed identity operator role

--- terraform/managed-identity.tf
+++ terraform/managed-identity.tf
@@ -11,7 +11,24 @@ resource "azurerm_user_assigned_identity" "app-identity" {
 resource "azurerm_key_vault_access_policy" "app-identity-vault-access" {
   key_vault_id = azurerm_key_vault.winc-net-app-vault.id
   tenant_id    = var.azure-tenant-id
-  object_id    = var.managed-identity-object-id # TODO:  Determine how to get this after creating the identity
+  object_id    = azurerm_user_assigned_identity.app-identity.principal_id # var.managed-identity-object-id # TODO:  Determine how to get this after creating the identity az identity show -g rg-mxinfo-wimc-net-dev -n mx-mxinfo-wimc-net-dev
 
   secret_permissions = ["Get", "List"]
 }
+
+
+
+data "azurerm_subscription" "primary" {
+
+}
+
+
+
+resource "azurerm_role_assignment" "app-identity-role" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Managed Identity Operator"
+  principal_id         = azurerm_user_assigned_identity.app-identity.principal_id
+}
+
+
+
