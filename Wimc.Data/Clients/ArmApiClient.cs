61ea0ccddd5e0be62a5cd1ad0d961490732ec490
Dan Maxim <dmaxim@mxinformatics.com>
2021-07-06 11:48:08 -0600

    Add list of api versions

--- Wimc.Data/Clients/ArmApiClient.cs
+++ Wimc.Data/Clients/ArmApiClient.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -10,8 +11,20 @@ namespace Wimc.Data.Clients
 {
     public class ArmApiClient : IApiClient
     {
-        private readonly ArmApiClientConfig _apiConfiguration; 
+        private readonly ArmApiClientConfig _apiConfiguration;
 
+        private readonly IList<string> _apiVersions  = new List<string>()
+        {
+            "2021-05-01",
+            "2021-03-01",
+            "2021-02-01",
+            "2020-11-01",
+            "2020-09-01",
+            "2020-08-01",
+            "2016-10-01",
+            "2016-01-01",
+            "2015-06-15",
+        };
         public ArmApiClient(HttpClient httpClient, IOptions<ArmApiClientConfig> configuration)
         {
             HttpClient = httpClient;
@@ -31,7 +44,6 @@ namespace Wimc.Data.Clients
             {
                 throw new MxNotFoundException($"Resource {resourceId} was not found");
             }
-
             return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
         }
 
@@ -50,5 +62,32 @@ namespace Wimc.Data.Clients
 
 
         }
+
+        private async Task<string> GetResource(string resourceId)
+        {
+            var lastResponse = string.Empty;
+            foreach (var apiVersion in _apiVersions)
+            {
+                using var request =
+                    new HttpRequestMessage(HttpMethod.Get, $"{resourceId}?api-version={apiVersion}");
+            
+                using var response = await HttpClient.SendAsync(request).ConfigureAwait(false);
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    throw new MxNotFoundException($"Resource {resourceId} was not found");
+                }
+
+                lastResponse =await response.Content.ReadAsStringAsync().ConfigureAwait(false); 
+                if (response.StatusCode != HttpStatusCode.BadRequest)
+                {
+                    return lastResponse;
+                }
+                
+            }
+
+            return lastResponse;
+
+        }
+        
     }
 }
