22d5f58d2de7fafff9faf598ff1f4e180c56dde6
Dan Maxim <dmaxim@mxinformatics.com>
2021-07-07 11:25:04 -0600

    Change to compact JSON formatter

--- Wimc/Startup.cs
+++ Wimc/Startup.cs
@@ -52,6 +52,7 @@ namespace Wimc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
+            logger.LogWarning("In startup");
             app.UseCustomExceptionHandler("Wimc-net", "/home/error");
             
             app.UseStaticFiles();
