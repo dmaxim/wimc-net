7cac3d0eea1f1549bb9544e213c41da6e2af03ae
Dan Maxim <dmaxim@mxinformatics.com>
2021-08-02 11:38:01 -0600

    Save resource added events to table storage

--- Wimc.Audit.EventHandling/ResourceAdded.cs
+++ Wimc.Audit.EventHandling/ResourceAdded.cs
@@ -1,3 +1,7 @@
+using System.Security.Cryptography;
+using System.Text;
+
+
 namespace Wimc.Audit.EventHandling
 {
     public class ResourceAdded
@@ -5,5 +9,21 @@ namespace Wimc.Audit.EventHandling
         public int ResourceContainerId { get; set; }
         public string CloudId { get; set; }
         public string Name { get; set; }
+
+        public string RowKey
+        {
+            get
+            {
+                using var hashAlgorithm = MD5.Create();
+                var hashBytes = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(this.CloudId));
+                var hashOutput = new StringBuilder(50);
+                foreach (var byteValue in hashBytes)
+                {
+                    hashOutput.Append(byteValue.ToString("x2"));
+                }
+
+                return hashOutput.ToString();
+            }
+        }
     }
 }
--- Wimc.Audit.EventHandling/ResourceAddedTrigger.cs
+++ Wimc.Audit.EventHandling/ResourceAddedTrigger.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Azure.Data.Tables;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using TableEntity = Azure.Data.Tables.TableEntity;
+
 
 namespace Wimc.Audit.EventHandling
 {
@@ -12,19 +16,65 @@ namespace Wimc.Audit.EventHandling
         public static async Task RunAsync([ServiceBusTrigger("resourceadded", Connection = "ServiceBusConnection")]
             string resourceAddedEvent, ILogger log)
         {
+                
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {resourceAddedEvent}");
 
              var resourceAddedCloudEvent = JsonConvert.DeserializeObject<ResourceAddedCloudEvent>(resourceAddedEvent);
 
-             ProcessResourceAddedEvent(resourceAddedCloudEvent);
+             await ProcessResourceAddedEvent(resourceAddedCloudEvent).ConfigureAwait(false);
 
         }
 
 
-        private static void ProcessResourceAddedEvent(ResourceAddedCloudEvent cloudEvent)
+        private static async Task ProcessResourceAddedEvent(ResourceAddedCloudEvent cloudEvent)
         {
-            
-            // Write resource added to data store
+            var accountName = System.Environment.GetEnvironmentVariable("accountName");
+            var accountKey = System.Environment.GetEnvironmentVariable("accountKey");
+            var endpointUrl = System.Environment.GetEnvironmentVariable("endpointUrl");
+            var tableName = System.Environment.GetEnvironmentVariable("tableName");
+
+            if (string.IsNullOrWhiteSpace(endpointUrl))
+            {
+                throw new ArgumentNullException(nameof(endpointUrl));
+            }
+            if (cloudEvent?.ResourceAdded == null)
+            {
+                return;
+            }
+
+            var tableClient = new TableClient(
+                new Uri(endpointUrl),
+                tableName,
+                new TableSharedKeyCredential(accountName,
+                    accountKey)
+            );
+
+            if (!EntityExists(cloudEvent, tableClient))
+            {
+                await AddEvent(cloudEvent, tableClient).ConfigureAwait(false);
+
+            }
+        }
+
+        private static bool EntityExists(ResourceAddedCloudEvent cloudEvent, TableClient tableClient)
+        {
+            var result =
+                tableClient.Query<TableEntity>(
+                    $"PartitionKey eq '{cloudEvent.ResourceAdded.ResourceContainerId}' and RowKey eq '{cloudEvent.ResourceAdded.RowKey}'");
+
+            return result.Any();
+        }
+
+        private static async Task AddEvent(ResourceAddedCloudEvent cloudEvent, TableClient tableClient)
+        {
+            var resourceAdded = new TableEntity(cloudEvent.ResourceAdded.ResourceContainerId.ToString(), cloudEvent.ResourceAdded.RowKey )
+            {
+                { "ResourceAdded", cloudEvent.data}
+            };
+
+               await  tableClient.AddEntityAsync(resourceAdded).ConfigureAwait(false);
+
+
         }
         
     }
